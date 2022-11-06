60302cca9031af2d5cf79c657c1e32cd989d63db
Raph <cappiello.raphael@gmail.com>
2022-11-06 17:20:42 +0100

    array name, join with , for view

--- example.js
+++ example.js
@@ -9,25 +9,27 @@ app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/example.html');
 });
 
+
 io.on('connection', (socket) => {  
   let channel = "";
   let pseudo = "";
-  console.log('new connection: ' + socket.id);
   socket.on('new_co', data => {
-    socket.join(data.room);
-    io.to(data.room).emit('new_message', 'new user connected : '+data.pseudo);
-    channel = data.room;
+    socket.join(data.channel);
+    io.to(data.channel).emit('new_message', 'new user connected : '+data.pseudo);
+    channel = data.channel;
     pseudo = data.pseudo;
   });
+
   socket.on('is_typing', data => {
-    io.to(channel).emit('is_typing', { message: data, pseudo: pseudo});
+      io.to(channel).emit('is_typing', data);
   });
+
   socket.on('chat_message', data => {
     data =  pseudo+" : " + data
     socket.to(channel).emit('new_message', data);
   });
-
 });
+
 //https://socket.io/get-started/chat#getting-this-example
 server.listen(3000, () => {
   console.log('listening on *:3000');
